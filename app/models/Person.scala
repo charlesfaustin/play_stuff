70994b32f537511bff3d165e1fd1c9ab4b20dd2e
Charles <Charles@local.host>
2015-04-20 23:04:41 +0100

    change to model

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -3,7 +3,8 @@ package models
 import play.api.libs.json._
 import java.util.Date
 
-case class Person(name: String,  email: String, age: Int, eligible: Option[Boolean])//, adate: Date)
+//add uuid val
+case class Person(name: String,  email: String, age: Int, eligible: Option[Boolean]) //, adate: Date)
 
 object Person {
 
