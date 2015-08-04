be1ce17db23c04d4db2448ae2d4eb5d0349ce087
Charles <Charles@local.host>
2015-08-04 22:43:35 +0100

    some stuff

---
+++ fabfile.py
@@ -0,0 +1,121 @@
+from fabric.api import env, local, run, sudo
+
+import os
+
+env.user = 'ubuntu'
+env.key_filename = ['~/.ssh/chefbox.pem']
+
+
+env.hosts = ['52.18.71.70']
+project_root = "~/"
+
+
+#  hardcoded for now
+repo_root = os.path.join(project_root, 'play_stuff')
+
+
+def update():
+    sudo('apt-get -y update', pty=True)
+
+
+def upgrade():
+    sudo('apt-get -y upgrade', pty=True)
+
+
+def update_supervisor():
+    sudo('supervisorctl reread && supervisorctl update')
+
+
+def install_nginx():
+    sudo('apt-get install nginx -y')
+
+
+def restart_ngninx():
+    sudo('service nginx restart')
+
+
+def os_packages():
+    sudo('apt-get install -y openjdk-7-jdk  sox libsox-fmt-mp3 unzip git supervisor nginx', pty=True)
+    run('echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list')
+    sudo('apt-get update')
+    sudo('apt-get install -y  sbt  --force-yes')
+    sudo('apt-get install  -y scala')
+
+
+def install_activator():
+    run('cd ~/')
+    run('wget http://downloads.typesafe.com/typesafe-activator/1.3.5/typesafe-activator-1.3.5.zip')
+    run('unzip typesafe-activator-1.3.5.zip')
+    run('cd /usr/bin && sudo ln -s /home/ubuntu/activator-dist-1.3.5/activator activator')
+    
+
+
+def clone():
+    run('cd ~/')
+    run('git clone https://github.com/charlesfaustin/play_stuff.git')
+    run('cd play_stuff/ && git checkout music')
+
+
+
+def pull():
+    run('cd %s && git pull' % repo_root)
+
+
+def clean():
+    #run('cd %s && activator clean' % repo_root )
+    run(' activator clean' )
+
+
+
+def compile():
+    run('cd %s && activator compile' % repo_root)
+
+
+def runserver():
+    run('cd %s && activator run' % repo_root)
+
+
+def stage():
+    run('cd %s && activator clean stage' % repo_root)
+
+
+
+def make_logs():
+    run("touch /home/ubuntu/play_app.log")
+    sudo("touch /home/ubuntu/nginx-access.log")
+    sudo("touch /home/ubuntu/nginx-error.log")
+
+
+def nginx_conf():
+    sudo("cp %s /etc/nginx/sites-available" % (repo_root + '/deploy/slyck'))
+    try:
+        sudo("rm /etc/nginx/sites-enabled/default")
+    except:
+        pass
+    sudo("ln -s /etc/nginx/sites-available/slyck /etc/nginx/sites-enabled/slyck")
+
+
+def supervisor_conf():
+    sudo("cp %s /etc/supervisor/conf.d" % (repo_root + '/deploy/slyck.conf'))
+
+
+def nginx_restart():
+    sudo('service nginx restart')
+
+
+def app_restart():
+    sudo("supervisorctl restart testapp")
+
+
+def initial_install():
+    update()
+    upgrade()
+    os_packages()
+    install_activator()
+    clone()
+    pull()
+
+
+
+
+
